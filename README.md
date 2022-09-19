d8aa57d56211a2081125fd243126d12196e33638
GuilhermeAlbring <guilhuerme_albring@hotmail.com>
2022-09-19 12:56:44 -0300

    "Commit entrega1"

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ProjetoTintas
+
+O projeto fara uma loja online que vendera e entregara produtos relacionado a pintura de varios estilos.
--- config/database.php
+++ config/database.php
@@ -48,9 +48,9 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'projeto_tintas'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -126,7 +126,7 @@ return [
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
-        'default' => [
+        'default' => env ('DB_CONNECTION','mysql')[
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
